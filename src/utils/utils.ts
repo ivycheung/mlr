b22c281e39889997ad0f5a91f266433d162a93da
Ivy Cheung <icyivy@hotmail.com>
2025-02-08 15:45:14 -0800

    Refactoring Session Data Table and Pitch Swing Chart into their own components

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,13 @@
 import { FormSchemaPitch } from "../types/schemas/pitch-schema";
 
-export function calculateDiff(pitch: FormSchemaPitch){
-const difference = pitch.swing - pitch.pitch;
-return difference < 0 ? 1000 + difference : difference
+export function calculateDiff(pitch: FormSchemaPitch) {
+    const difference = pitch.swing - pitch.pitch;
+    return difference < 0 ? 1000 + difference : difference
 }
 
-export function calculateDelta(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null ) {
-const delta = pitch.pitch - (previousPitch?.pitch ?? 0)
-return delta < 0 ? 1000 + delta : delta
+export function calculateDelta(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
+    const delta = pitch.pitch - (previousPitch?.pitch ?? 0)
+    return delta < 0 ? 1000 + delta : delta
 
 }
 
@@ -20,7 +20,6 @@ export function calculateCircleDelta(pitch: FormSchemaPitch, previousPitch: Form
     }
 
     return delta
-
 }
 
 export function calculateHistogram(pitches: Array<FormSchemaPitch>, bucketSize: number) {
@@ -31,19 +30,17 @@ export function calculateHistogram(pitches: Array<FormSchemaPitch>, bucketSize: 
             bins[index]++;
         }
     });
+
     return bins;
 }
 
 export function getNextPitch(pitches: Array<FormSchemaPitch>, index: number) {
-    // const currentPitch = pitches[index].pitch;
-    return (pitches[index + 1] != undefined ? pitches[index +1].pitch : '-');
+    return (pitches[index + 1] != undefined ? pitches[index + 1].pitch : '-');
 }
 
 export function getDelta(pitches: Array<FormSchemaPitch>, index: number) {
     const currentPitch = (pitches[index] != undefined ? pitches[index].pitch : null);
     const previousPitch = (pitches[index - 1] != undefined ? pitches[index - 1].pitch : null);
-    console.log(currentPitch, previousPitch);
-    
 
     if (currentPitch == undefined || currentPitch == null || previousPitch == undefined || previousPitch == null) {
         return '-';
@@ -51,141 +48,4 @@ export function getDelta(pitches: Array<FormSchemaPitch>, index: number) {
     else {
         return Math.abs(previousPitch - currentPitch);
     }
-}
-
-export function getModel1(pitch: FormSchemaPitch) {
-const average = (pitch.pitch + pitch.swing) / 2;
-const adjustment = Math.abs(pitch.pitch - pitch.swing) > 500 ? 500 : 0;
-
-return Math.round((average + adjustment) % 1000)
-}
-
-export function getModel2(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let diff = calculateDiff(pitch)
-let delta = calculateDelta(pitch, previousPitch)
-
-const average = (diff + delta) / 2;
-const adjustment = Math.abs(diff - delta) > 500 ? 500 : 0;
-
-return Math.round((average + adjustment) % 1000)
-}
-
-export function getModel3(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let model1 = getModel1(pitch)
-let model2 = getModel2(pitch, previousPitch)
-
-let adjustment = model1 - model2 > 500 ? 500 : 0
-
-// Apply the formula
-return Math.round(((model1 + model2) / 2 + adjustment) % 1000);
-}
-
-export function getModel4(pitch: FormSchemaPitch) {
-let model1 = getModel1(pitch)
-
-if ((model1 + 500) > 1000) {
-    return model1 - 500;
-} else {
-    return model1 + 500;
-}
-}
-
-export function getModel5(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let model2 = getModel2(pitch, previousPitch)
-
-if ((model2 + 500) > 1000) {
-    return model2 - 500;
-} else {
-    return model2 + 500;
-}
-}
-
-export function getModel6(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let model3 = getModel3(pitch, previousPitch)
-
-if ((model3 + 500) > 1000) {
-    return model3 - 500;
-} else {
-    return model3 + 500;
-}
-}
-
-export function getModel7(pitch: FormSchemaPitch) {
-let diff = calculateDiff(pitch)
-
-if (diff > 750 || diff < 250) {
-    return getModel4(pitch);
-} else {
-    return getModel1(pitch);
-}
-}
-
-export function getModel8(pitch: FormSchemaPitch) {
-let diff = calculateDiff(pitch)
-
-if (diff > 750 || diff < 250) {
-    return getModel1(pitch);
-} else {
-    return getModel4(pitch);
-}
-}
-
-export function getModel9(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let diff = calculateDiff(pitch)
-
-if (diff > 750 || diff < 250) {
-    return getModel5(pitch, previousPitch);
-} else {
-    return getModel2(pitch, previousPitch);
-}
-}
-
-export function getModel10(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let diff = calculateDiff(pitch)
-
-if (diff > 750 || diff < 250) {
-    return getModel2(pitch, previousPitch);
-} else {
-    return getModel5(pitch, previousPitch);
-}
-}
-
-export function getModel11(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let diff = calculateDiff(pitch)
-
-if (diff > 750 || diff < 250) {
-    return getModel6(pitch, previousPitch);
-} else {
-    return getModel3(pitch, previousPitch);
-}
-}
-
-export function getModel12(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let diff = calculateDiff(pitch)
-
-if (diff > 750 || diff < 250) {
-    return getModel3(pitch, previousPitch);
-} else {
-    return getModel6(pitch, previousPitch);
-}
-}
-
-export function getModel13(pitch: FormSchemaPitch) {
-return calculateDiff(pitch)
-}
-
-export function getModel14(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-return calculateDelta(pitch, previousPitch)
-}
-
-export function getModel15(pitch: FormSchemaPitch) {
-let diff = calculateDiff(pitch)
-
-return diff > 500 ? diff - 500 : diff + 500;
-}
-
-export function getModel16(pitch: FormSchemaPitch, previousPitch: FormSchemaPitch | null) {
-let delta = calculateDelta(pitch, previousPitch)
-
-return delta > 500 ? delta - 500 : delta + 500;
 }
